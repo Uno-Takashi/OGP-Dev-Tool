ee3fd880e251c8111789c73d2f847bba142c08e1
Takashi Uno <euno.eng@gmail.com>
2023-02-15 18:19:14 +0900

    init

--- src/devtools_panel.tsx
+++ src/devtools_panel.tsx
@@ -157,6 +157,21 @@ const Panel = () => {
                         </Link>
                     </Grid>
                 </Grid>
+                <Grid xs={12} md={12}>
+                    <Typography variant="subtitle1" component="h3">
+                        <LaptopIcon sx={{ verticalAlign: "middle" }} ></LaptopIcon>
+                        Summary with Large Image Card (PC)
+                    </Typography>
+                    <div className="twitter_summary_large_image_large">
+                        <div className="preview_img" style={{ background: get_background_url() }}>
+                        </div>
+                        <div className="preview_text">
+                            <p className="preview_url">{get_origin()}</p>
+                            <p className="preview_title">{get_title()}</p>
+                            <p className="preview_description">{get_description()}</p>
+                        </div>
+                    </div>
+                </Grid>
 
                 <Grid container>
                     <Grid xs={12} md={6}>
@@ -185,21 +200,6 @@ const Panel = () => {
                             <div className="preview_text">
                                 <p className="preview_url">{get_origin()}</p>
                                 <p className="preview_title">{get_title()}</p>
-                            </div>
-                        </div>
-                    </Grid>
-                    <Grid xs={12} md={6}>
-                        <Typography variant="subtitle1" component="h3">
-                            <LaptopIcon sx={{ verticalAlign: "middle" }} ></LaptopIcon>
-                            Summary with Large Image Card (PC)
-                        </Typography>
-                        <div className="twitter_summary_large_image_large">
-                            <div className="preview_img" style={{ background: get_background_url() }}>
-                            </div>
-                            <div className="preview_text">
-                                <p className="preview_url">{get_origin()}</p>
-                                <p className="preview_title">{get_title()}</p>
-                                <p className="preview_description">{get_description()}</p>
                             </div>
                         </div>
                     </Grid>
